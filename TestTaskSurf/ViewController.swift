29748e1475ba7128c9dc60ca3865b67b40173e0d
Anton Denisyuk <vretiel@icloud.com>
2023-02-09 14:47:29 +0300

    initial commit

---
+++ TestTaskSurf/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  TestTaskSurf
+//
+//  Created by Антон Денисюк on 09.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemIndigo
+    }
+}
